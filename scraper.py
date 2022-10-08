f8afb00caf2cef950dc5182725f7c78fb38a0568
Ali Murtaza Ayubi <info@aliayubi.com>
2022-10-09 00:18:17 +0200

    Writing comments, changing some variable names, create variable for URL

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+# import dependencies
 import requests
 from bs4 import BeautifulSoup
 import json
@@ -5,6 +6,7 @@ import os
 import psycopg2
 from configparser import ConfigParser
 
+# Article object for saving and viewing articles
 class Article:
     def __init__(self,title,shortContent,content,image):
         self.title = title
@@ -12,6 +14,7 @@ class Article:
         self.content = content
         self.image = image
 
+    #save article as JSON file
     def saveToJson(self):
         data = {'title': self.title, 'shortContent': self.shortContent, 'content': self.content, 'image': self.image}
         fileName = 'data.json'
@@ -26,6 +29,7 @@ class Article:
             with open(fileName, 'w') as file:
                 json.dump([data],file)
 
+    # Save article to DB
     def save(self):
         parser = ConfigParser()
         parser.read('database.ini')
@@ -45,6 +49,7 @@ class Article:
         connectToDB.close()
         print(cursor)
 
+    # Getting article data from DB
     def view(self, title = ''):
         parser = ConfigParser()
         parser.read('database.ini')
@@ -64,27 +69,33 @@ class Article:
         fetchDatas = cursor.fetchall()
         print(fetchDatas)
 
+# Scraper class
 class Scraper:
     def __init__(self,url):
         self.url = url
 
+    # get body of the scraped url
     def getBody(self):
         body = requests.get(self.url)
         parse = BeautifulSoup(body.content,'html.parser')
         return parse
 
-scraper = Scraper('https://aliayubi.com/blog/')
-body = scraper.getBody()
+# Let's start the scraper 
+url = 'https://aliayubi.com/blog/' # Url that we want to scrape
+scrape = Scraper(url)
+body = scrape.getBody()
 
-articles = body.find_all(class_="et_pb_post")
+articles = body.find_all(class_="et_pb_post") # Select the contents div that you want to scrape
 
+# Function to get the first & only text inside a nested element
 def getJustCurrentElText(parent):
     return ''.join(parent.find_all(text=True, recursive=False)).strip()
 
-for content in articles:
-    title = content.find(class_="entry-title").text
-    shortContent = getJustCurrentElText(content)
-
+# Loop through contents divs
+for article in articles:
+    shortContent = getJustCurrentElText(article)
+    title = article.find(class_="entry-title").text
+    
     articleLink = content.find(class_="entry-title").find('a')['href']
     articleBody = Scraper(articleLink).getBody()
     content = articleBody.find(class_='entry-content').text
